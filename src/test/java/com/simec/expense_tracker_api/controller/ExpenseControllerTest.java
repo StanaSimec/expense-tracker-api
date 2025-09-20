06c5c2328ab6b5c75a3bfb105d8b74041ef33ddf
standasimecek <simecek.stanis@gmail.com>
2025-09-20 22:16:48 +0200

    addded expense controller test for update, create and delete

--- src/test/java/com/simec/expense_tracker_api/controller/ExpenseControllerTest.java
+++ src/test/java/com/simec/expense_tracker_api/controller/ExpenseControllerTest.java
@@ -1,5 +1,7 @@
 package com.simec.expense_tracker_api.controller;
 
+import com.simec.expense_tracker_api.dao.CategoryDao;
+import com.simec.expense_tracker_api.dto.ExpenseRequestDto;
 import com.simec.expense_tracker_api.dto.ExpenseResponseDto;
 import com.simec.expense_tracker_api.entity.Category;
 import com.simec.expense_tracker_api.entity.Expense;
@@ -14,44 +16,111 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ExpenseController.class)
 class ExpenseControllerTest {
 
+    private static final Expense EXPENSE = new Expense(1, "Espresso",
+            LocalDate.of(2024, 2, 4),
+            new Category(1, "Coffe"));
+    private static final String URL = "/v1/expenses";
+    private static final String REQUEST_CONTENT = """
+            {
+                "name": "%s",
+                "appliedAt": "%s",
+                "category": "%s"
+            }
+            """.formatted(
+            EXPENSE.name(),
+            EXPENSE.appliedAt().format(DateTimeFormatter.ISO_LOCAL_DATE),
+            EXPENSE.category().name());
     @Autowired
     private MockMvc mockMvc;
-
     @MockitoBean
     private ExpenseService expenseService;
-
     @MockitoBean
     private ExpenseResponseDtoMapper mapper;
-
     @MockitoBean
     private FilterService filterService;
+    @MockitoBean
+    private CategoryDao categoryDao;
 
     @WithMockUser
     @Test
-    void expenseExistsWhenDetailIsRequestedThenExpenseIsReturned() throws Exception {
-        Expense expense = new Expense(1, "Espresso", LocalDate.now(), new Category(1, "Coffe"));
+    void whenDetailIsRequestedThenExpenseIsReturned() throws Exception {
 
-        Mockito.when(expenseService.find(expense.id())).thenReturn(expense);
-        Mockito.when(mapper.toDto(expense)).thenReturn(
-                new ExpenseResponseDto(expense.id(), expense.name(), expense.appliedAt(), expense.category().name()));
+        Mockito.when(expenseService.find(EXPENSE.id())).thenReturn(EXPENSE);
+        Mockito.when(mapper.toDto(EXPENSE)).thenReturn(
+                new ExpenseResponseDto(EXPENSE.id(), EXPENSE.name(), EXPENSE.appliedAt(), EXPENSE.category().name()));
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/v1/expenses/" + expense.id()))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.id").value(expense.id()))
-                .andExpect(jsonPath("$.name").value(expense.name()))
-                .andExpect(jsonPath("$.appliedAt").value(expense.appliedAt().toString()))
-                .andExpect(jsonPath("$.category").value(expense.category().name()));
+        ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.get(URL + "/" + EXPENSE.id()))
+                .andExpect(status().isOk());
+        assertResponseBody(actions);
+    }
+
+    @WithMockUser
+    @Test
+    void whenPostIsSendThenCreatedExpenseIsReturned() throws Exception {
+
+        Mockito.when(categoryDao.findByName(EXPENSE.category().name())).thenReturn(Optional.of(EXPENSE.category()));
+        Mockito.when(expenseService.create(
+                        new ExpenseRequestDto(EXPENSE.name(), EXPENSE.appliedAt(), EXPENSE.category().name())))
+                .thenReturn(EXPENSE);
+        Mockito.when(mapper.toDto(EXPENSE)).thenReturn(
+                new ExpenseResponseDto(EXPENSE.id(), EXPENSE.name(), EXPENSE.appliedAt(), EXPENSE.category().name()));
+
+        ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.post(URL)
+                        .content(REQUEST_CONTENT)
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+        assertResponseBody(actions);
+    }
+
+    @WithMockUser
+    @Test
+    void whenUpdateIsSendThenUpdatedExpenseIsReturned() throws Exception {
+        Mockito.when(categoryDao.findByName(EXPENSE.category().name())).thenReturn(Optional.of(EXPENSE.category()));
+        Mockito.when(expenseService.update(
+                        new ExpenseRequestDto(EXPENSE.name(), EXPENSE.appliedAt(), EXPENSE.category().name()),
+                        EXPENSE.id()))
+                .thenReturn(EXPENSE);
+        Mockito.when(mapper.toDto(EXPENSE)).thenReturn(
+                new ExpenseResponseDto(EXPENSE.id(), EXPENSE.name(), EXPENSE.appliedAt(), EXPENSE.category().name()));
+
+        ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.put(URL + "/" + EXPENSE.id())
+                        .content(REQUEST_CONTENT)
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        assertResponseBody(actions);
+    }
+
+    @WithMockUser
+    @Test
+    void whenDeleteIsSendThenDeletedStatusIsReturned() throws Exception {
+        Mockito.when(expenseService.find(EXPENSE.id())).thenReturn(EXPENSE);
+        Mockito.when(mapper.toDto(EXPENSE)).thenReturn(
+                new ExpenseResponseDto(EXPENSE.id(), EXPENSE.name(), EXPENSE.appliedAt(), EXPENSE.category().name()));
+
+        mockMvc.perform(MockMvcRequestBuilders.delete(URL + "/" + EXPENSE.id()).with(csrf()))
+                .andExpect(status().isNoContent());
+    }
+
+    private void assertResponseBody(ResultActions resultActions) throws Exception {
+        resultActions.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(EXPENSE.id()))
+                .andExpect(jsonPath("$.name").value(EXPENSE.name()))
+                .andExpect(jsonPath("$.appliedAt").value(EXPENSE.appliedAt().toString()))
+                .andExpect(jsonPath("$.category").value(EXPENSE.category().name()));
     }
 }
